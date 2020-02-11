bc9ef376429ec7f38325d4895b3f0b3f06edc900
Alejandro Molina <amolinasalazar@gmail.com>
2020-02-11 20:19:22 +0100

    UPDATE implements BaseHashTable

--- HashTable.java
+++ HashTable.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class HashTable<K, V>{
+public class HashTable<K, V> implements BaseHashTable<K, V>{
 
     private static final int DEFAULT_SIZE = 6;
     private final List<LinkedList<V>> values;
@@ -17,6 +17,7 @@ public class HashTable<K, V>{
         }
     }
 
+    @Override
     public void put(K key, V value){
         int hashCode = key.hashCode();
         int hashKey = getKey(hashCode);
@@ -26,6 +27,24 @@ public class HashTable<K, V>{
         }
 
         this.values.get(hashKey).add(value);
+    }
+
+    @Override
+    public V get(K key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
     }
 
     private int getKey(int hashCode){
