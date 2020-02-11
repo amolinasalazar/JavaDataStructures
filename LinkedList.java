83ae99011c49dee54f8840cee3882ac8a1f6f501
Alejandro Molina <amolinasalazar@gmail.com>
2020-02-11 20:49:12 +0100

    ADD LinkedList class implementing BaseLinkedList

---
+++ LinkedList.java
@@ -0,0 +1,27 @@
+public class LinkedList<E> implements BaseLinkedList<E>{
+
+    @Override
+    public void add(E value) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public E get(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public E remove(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
