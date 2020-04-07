3cb2fbe58cc5f52ff1176152cc9ea633b1b2c894
Alejandro Molina <amolinasalazar@gmail.com>
2020-04-07 17:27:37 +0200

    REMOVE unnecessary methods from extended interface

--- java-data-structures/src/main/java/com/codepull/javadatastructures/Interfaces/LinkedList/ExtendedLinkedList.java
+++ java-data-structures/src/main/java/com/codepull/javadatastructures/Interfaces/LinkedList/ExtendedLinkedList.java
@@ -1,12 +1,6 @@
 package com.codepull.javadatastructures.Interfaces.LinkedList;
 
 public interface ExtendedLinkedList<E> {
-
-	void add(E value);
-	E get(int index);
-	E remove();
-	int size();
-
 	void add(int index, E element);
 	E remove(int index);
 	void clear();
